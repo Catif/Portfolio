ec538cd15bafb2e111f380725d74202ad41cfc0e
Catif <bradley.barbier@outlook.fr>
2023-01-23 23:04:01 +0100

    Delete basic code

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,9 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
+<script setup></script>
 
-<template>
-  <main>
-    <TheWelcome />
-  </main>
-</template>
+<template></template>
+
+<style lang="scss">
+body {
+  background-color: red;
+}
+</style>
