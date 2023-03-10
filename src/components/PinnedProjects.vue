16ea916aa73d4116d9e308c88326372a67c4af1e
Bradley BARBIER <bradley.barbier@outlook.fr>
2023-03-10 18:59:16 +0100

    Improve description and add real Picture

--- src/components/PinnedProjects.vue
+++ src/components/PinnedProjects.vue
@@ -9,25 +9,25 @@ const projects = reactive([
 		description:
 			"You can stream like Twitch or Youtube, but this project is more focused on the protection of data.<br> You can record your videos on the cloud and watch it later, it is a security to not lost your video if you are in a bad position.",
 		technologies: ["Slim", "MySQL", "VueJS", "WebRTC", "Docker", '(All technologies of "Streaming with web camera" project below)'],
-		picture: "https://picsum.photos/1920/1080",
+		picture: "https://i.imgur.com/3ZQRuxT.png",
 		github: "https://github.com/Catif/LP-PTUT_Scudo",
 	},
 	{
 		id: 2,
 		name: "Streaming with web camera",
 		description:
-			'This is a project that allows you to stream your webcam with replay video.<br> That is the small part of a <a href="https://github.com/Catif/LP-PTUT_Scudo" target="_blank">bigger project</a> that I am working on at school.',
+			'This is a project that allows you to stream your webcam with replay video store on server.<br> That is the small part of a <a href="https://github.com/Catif/LP-PTUT_Scudo" target="_blank">bigger project</a> that I am working on at school.',
 		technologies: ["NodeJS", "RTCMulticonnection", "Socket.io", "WebRTC", "Docker"],
-		picture: "https://picsum.photos/1920/1080",
+		picture: "https://i.imgur.com/jBLx12H.gif",
 		github: "https://github.com/Catif/Record-Live-WebRTC",
 	},
 	{
 		id: 3,
 		name: "Atelier 1 - Photomedia",
 		description:
-			'You know <a href="https://www.pinterest.fr/">Pinterest</a> ? This is a little clone of it. You can publish your own pictures and watch other people\'s pictures.',
+			"You know <a href=\"https://www.pinterest.fr/\">Pinterest</a> ? This is a little clone of it. You can publish your own pictures and watch other people's pictures.<br> You can create galeries or join other people's galeries and being a contributor of it.",
 		technologies: ["PHP", "JavaScript", "SASS", "MySQL"],
-		picture: "https://picsum.photos/1920/1080",
+		picture: "https://i.imgur.com/htfTxvO.jpeg",
 		github: "https://github.com/Catif/LP1-CIASIE_Atelier1",
 	},
 ]);
