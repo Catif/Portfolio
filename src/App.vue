d5e590bec73ab20aeccf6bc9f9b4a571e762c197
Bradley BARBIER <bradley.barbier@outlook.fr>
2023-03-14 23:28:59 +0100

    add transition

--- src/App.vue
+++ src/App.vue
@@ -14,12 +14,30 @@ provide("confetti", confetti);
 		<Navbar />
 	</header>
 
-	<main class="container">
-		<RouterView />
-	</main>
+	<router-view v-slot="{ Component, route }">
+		<transition :name="route.meta.transition">
+			<component :is="Component" />
+		</transition>
+	</router-view>
 </template>
 
 <style lang="scss">
 @import "@/assets/scss/reset.scss";
 @import "@/assets/scss/basic.scss";
+
+/* Animation de transition de page */
+.page-transition-slide-right-enter-active,
+.page-transition-slide-right-leave-active,
+.page-transition-slide-left-enter-active,
+.page-transition-slide-left-leave-active {
+	position: absolute;
+	transition: all 10s ease-in;
+}
+
+.page-transition-slide-right-enter-from,
+.page-transition-slide-left-leave-to,
+.page-transition-slide-left-enter-from,
+.page-transition-slide-right-leave-to {
+	opacity: 0;
+}
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,29 +2,55 @@ import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "@/views/HomeView.vue";
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: "/",
-      name: "home",
-      component: HomeView,
-    },
-    {
-      path: "/portfolio",
-      name: "portfolio",
-      component: () => import("@/views/PortfolioView.vue"),
-    },
-    {
-      path: "/blog",
-      name: "blog",
-      component: () => import("@/views/BlogView.vue"),
-    },
-    {
-      path: "/contact",
-      name: "contact",
-      component: () => import("@/views/ContactView.vue"),
-    },
-  ],
+	history: createWebHistory(import.meta.env.BASE_URL),
+	routes: [
+		{
+			path: "/",
+			name: "home",
+			component: HomeView,
+			meta: {
+				transition: "page-transition-slide-left",
+			},
+		},
+		{
+			path: "/portfolio",
+			name: "portfolio",
+			component: () => import("@/views/PortfolioView.vue"),
+			meta: {
+				transition: "page-transition-slide-left",
+			},
+		},
+		{
+			path: "/blog",
+			name: "blog",
+			component: () => import("@/views/BlogView.vue"),
+			meta: {
+				transition: "page-transition-slide-left",
+			},
+		},
+		{
+			path: "/contact",
+			name: "contact",
+			component: () => import("@/views/ContactView.vue"),
+			meta: {
+				transition: "page-transition-slide-left",
+			},
+		},
+	],
+});
+
+router.afterEach((to, from) => {
+	const toIndex = router.options.routes.findIndex((r) => r.path === to.path);
+	const fromIndex = router.options.routes.findIndex((r) => r.path === from.path);
+	to.meta.transition = toIndex < fromIndex ? "page-transition-slide-right" : "page-transition-slide-left";
+
+	console.log(to.meta.transition);
+
+	// scoll top smoothly
+	window.scrollTo({
+		top: 0,
+		behavior: "smooth",
+	});
 });
 
 export default router;
