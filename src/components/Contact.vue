4952f9563787f55336d2b10453d34a9d8b04c8f2
Bradley BARBIER <bradley.barbier@outlook.fr>
2023-03-17 16:29:23 +0100

    Created Contact Component

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -2,7 +2,15 @@
 
 <template>
 	<section id="Contact">
-		<h2>Contact</h2>
+		<p>
+			You can contact me at
+			<a href="mailto:bradley.barbier@outlook.fr" target="_blank"> bradley.barbier@outlook.fr</a>
+		</p>
+
+		<p>
+			or by
+			<a href="https://www.linkedin.com/in/bradley-barbier/" target="_blank">LinkedIn</a>
+		</p>
 	</section>
 </template>
 
