2142ddda8eb88fa9bc0bef788690fd863f8e9a94
Bradley BARBIER <bradley.barbier@outlook.fr>
2023-07-02 01:32:58 +0200

    Delete video

--- src/components/Blog.vue
+++ src/components/Blog.vue
@@ -1,18 +1,12 @@
 <script setup></script>
 
 <template>
-	<section id="Blog">
-		<h2>{{ $t("blog.title") }}</h2>
-		<iframe
-			width="560"
-			height="315"
-			src="https://www.youtube-nocookie.com/embed/nZcvgJIMvYw"
-			title="YouTube video player"
-			frameborder="0"
-			allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-			allowfullscreen
-		></iframe>
-	</section>
+  <section id="Blog">
+    <h2>{{ $t("blog.title") }}</h2>
+    <p>
+      TODO
+    </p>
+  </section>
 </template>
 
 <style lang="scss"></style>
