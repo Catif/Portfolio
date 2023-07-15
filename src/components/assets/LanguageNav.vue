f3beeff58c41e93ef0122850242847ec72ad4986
Bradley BARBIER <bradley.barbier@outlook.fr>
2023-07-15 23:16:18 +0200

    Send event when changing language

--- src/components/assets/LanguageNav.vue
+++ src/components/assets/LanguageNav.vue
@@ -1,19 +1,28 @@
 <script setup>
+import { inject } from 'vue'
 import { useRoute } from 'vue-router'
 
+const bus = inject('bus')
 const route = useRoute()
+
+function changeLanguage(lang) {
+  if (lang === 'fr')
+    bus.emit('changeLanguage', 'fr')
+  else
+    bus.emit('changeLanguage', 'en')
+}
 </script>
 
 <template>
   <div id="choiceLanguage">
-    <template v-if="route.params.lang === 'fr'">
-      <router-link :to="{ name: 'home', params: { lang: 'en' } }">
-        <img src="/img/locales/en.webp">
+    <template v-if="route.params.lang === 'en'">
+      <router-link :to="{ name: 'home', params: { lang: 'fr' } }" @click="changeLanguage('fr')">
+        <img src="/img/locales/fr.webp">
       </router-link>
     </template>
     <template v-else>
-      <router-link :to="{ name: 'home', params: { lang: 'fr' } }">
-        <img src="/img/locales/fr.webp">
+      <router-link :to="{ name: 'home', params: { lang: 'en' } }" @click="changeLanguage('en')">
+        <img src="/img/locales/en.webp">
       </router-link>
     </template>
   </div>
