e9954e319d5a932e5c8ec0a974854cbea7a9d85b
Bradley BARBIER <bradley.barbier@outlook.fr>
2023-07-20 11:37:11 +0200

    delete component Blog to just use nativ inside view

--- src/views/BlogView.vue
+++ src/views/BlogView.vue
@@ -1,9 +1,46 @@
 <script setup>
-import Blog from '@/components/Blog/Blog.vue'
+import { inject, onMounted, ref } from 'vue'
+
+import BlogCard from '@/components/Blog/BlogCard.vue'
+
+const api = inject('api')
+const blogs = ref([])
+
+function getBlogs() {
+  api.get('/items/blog?fields=*,translations.*&sort=-id')
+    .then(response => response.data)
+    .then((data) => {
+      data = data.data
+
+      blogs.value = data
+    })
+}
+
+onMounted(() => {
+  getBlogs()
+})
 </script>
 
 <template>
-  <Blog />
+  <section id="Blog">
+    <h2>{{ $t("blog.title") }}</h2>
+    <div class="list-blog">
+      <template v-for="blog in blogs" :key="blog.blog_id">
+        <BlogCard :blog="blog" />
+      </template>
+    </div>
+  </section>
 </template>
 
-<style lang="scss"></style>
+<style lang="scss">
+#Blog {
+  .list-blog {
+    display: flex;
+    flex-direction: column;
+    gap: 3rem;
+
+    width: 100%;
+    max-width: 700px;
+  }
+}
+</style>
